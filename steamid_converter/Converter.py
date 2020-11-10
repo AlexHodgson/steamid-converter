90c7f55b9eaa774c75bac5aa78c05753ea482fc1
Alex Hodgson <56037599+AlexHodgson@users.noreply.github.com>
2020-11-10 17:47:35 +0000

    Implement to_steamID

--- steamid_converter/Converter.py
+++ steamid_converter/Converter.py
@@ -1,7 +1,43 @@
 import re
+import math
 
 def to_steamID(steamID):
-    pass
+    """Convert to steamID from steamID3 or steamID64
+
+    Parameters:
+    int/str : steamID - Steam id to convert
+    """
+
+    id_str = str(steamID)
+
+    if re.search("^\[.*\]$", id_str): # If passed steamID3
+
+        id_split = id_str.split(":") # Split string into 'Universe', Account type, and Account number
+        account_id3 = int(id_split[2][:-1]) # Remove ] from end of steamID3
+
+        if account_id3 % 2 == 0:
+            account_type = 0
+        else:
+            account_type = 1
+
+        account_id = (account_id3 - account_type) // 2
+
+    elif id_str.isnumeric(): # Passed steamID64
+
+        check_steamID64_length(id_str) # Validate id passed in
+
+        id64_base = 76561197960265728 # steamID64 are all offset from this value
+        offset_id = int(id_str) - id64_base
+
+        # Get the account type and id
+        if offset_id % 2 == 0:
+            account_type = 0
+        else:
+            account_type = 1
+
+        account_id = ((offset_id - account_type) // 2)
+
+    return "STEAM_0:" + str(account_type) + ":" + str(account_id)
 
 def to_steamID3(steamID):
     """Convert to steamID3 from steamID or steamID64
@@ -19,32 +55,37 @@ def to_steamID3(steamID):
         account_type = int(id_split[1]) # Check for account type
         account_id = int(id_split[2]) # Account number, needs to be doubled when added to id3
 
-        return "[U:1:" + str((account_id * 2) - account_type) + "]"
-
-
-
+        # Join together in steamID3 format
+        return "[U:1:" + str(((account_id + account_type) * 2) - account_type) + "]"
 
     elif id_str.isnumeric(): # Passed steamID64
+        
+        check_steamID64_length(id_str) # Validate id passed in
 
         id64_base = 76561197960265728 # steamID64 are all offset from this value
-
         offset_id = int(id_str) - id64_base
 
+        # Get the account type and id
         if offset_id % 2 == 0:
             account_type = 0
-            account_id = offset_id // 2
         else:
             account_type = 1
-            account_id = ((offset_id - 1) // 2) + 1
 
+        account_id = ((offset_id - account_type) // 2) + account_type
+
+        # Join together in steamID3 format
         return "[U:1:" + str((account_id * 2) - account_type) + "]"
+        
+    else:
+        raise ValueError(f"Unable to decode steamID: {steamID}")
 
 
-def to_steamID64(steamID):
+def to_steamID64(steamID, as_int = False):
     """Convert to steamID64 from steamID or steamID3
 
     Parameters:
     int/str : steamID - Steam id to convert
+    Bool  : as_int - If the steamID64 is returned in integer format, Default = False
     """
 
     id_str = str(steamID)
@@ -56,24 +97,35 @@ def to_steamID64(steamID):
         account_type = int(id_split[1]) # Check for account type
         account_id = int(id_split[2]) # Account number, needs to be doubled when added to id64
 
-        return id64_base + (account_id * 2) + account_type
-
     elif re.search("^\[.*\]$", id_str): # If passed steamID3
 
         account_id3 = int(id_split[2][:-1]) # Remove ] from end of steamID3
 
         if account_id3 % 2 == 0:
             account_type = 0
-            account_id = account_id3 // 2
         else:
             account_type = 1
-            account_id = (account_id3 - 1) // 2
 
-        return id64_base + (account_id * 2) + account_type
+        account_id = (account_id3 - account_type) // 2
 
     else:
         raise ValueError(f"Unable to decode steamID: {steamID}")
 
+
+    id64 = id64_base + (account_id * 2) + account_type
+
+    # Check if returning as string or integer
+    if as_int:
+        return id64
+    else:
+        return str(id64)
+
+
+def check_steamID64_length(id_str :str):
+    # All steamId64 should be 17 digits in length
+
+    if len(id_str) != 17:
+        raise ValueError(f"Incorrect length for steamID64: {id_str}")
 
 
 
--- tests/conversion_tests.py
+++ tests/conversion_tests.py
@@ -1,15 +1,27 @@
 import sys
-sys.path.append(r'A:\Documents\GitHub\steamid-converter\steamid_converter')
+sys.path.append(r'A:\Documents\GitHub\steamid-converter\steamid_converter') # TODO make this relative
 
 import unittest
 import Converter # Code being tested
 
 class TestConversions(unittest.TestCase):
+    # The accounts being tested represent both account types (0 and 1)
 
     def test_steamID64(self): # Test conversion to steamID64
 
         self.assertEqual(Converter.to_steamID64("[U:1:94253115]"), Converter.to_steamID64("STEAM_0:1:47126557")) # Corresponding steamID3 and SteamID
         self.assertEqual(Converter.to_steamID64("[U:1:11926250]"), Converter.to_steamID64("STEAM_0:0:5963125"))
+
+        self.assertEqual(Converter.to_steamID64("[U:1:94253115]"), "76561198054518843") # Test a known conversion
+        self.assertEqual(Converter.to_steamID64("[U:1:11926250]"), "76561197972191978")
+
+    def test_steamID3(self): # Test conversion to steamID3
+
+        self.assertEqual(Converter.to_steamID3("76561198054518843"), Converter.to_steamID3("STEAM_0:1:47126557")) # Corresponding steamID3 and SteamID
+        self.assertEqual(Converter.to_steamID3("76561197972191978"), Converter.to_steamID3("STEAM_0:0:5963125"))
+
+        self.assertEqual(Converter.to_steamID3("STEAM_0:0:5963125"), "[U:1:11926250]") # Test a known conversion
+        self.assertEqual(Converter.to_steamID3("STEAM_0:1:47126557"), "[U:1:94253115]")
 
 
 if __name__ == '__main__':

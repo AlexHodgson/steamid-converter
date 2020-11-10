f93bb0c1c5a5f485f7ac72d55f652c8362e18f8f
Alex Hodgson <56037599+AlexHodgson@users.noreply.github.com>
2020-11-10 18:04:36 +0000

    Update conversion_tests.py
    Extended test coverage now all conversions are implemented

--- tests/conversion_tests.py
+++ tests/conversion_tests.py
@@ -5,24 +5,42 @@ import unittest
 import Converter # Code being tested
 
 class TestConversions(unittest.TestCase):
+    # Test that the id conversion functions are giving the correct results
     # The accounts being tested represent both account types (0 and 1)
 
-    def test_steamID64(self): # Test conversion to steamID64
+    def test_steamID(self):
 
-        self.assertEqual(Converter.to_steamID64("[U:1:94253115]"), Converter.to_steamID64("STEAM_0:1:47126557")) # Corresponding steamID3 and SteamID
-        self.assertEqual(Converter.to_steamID64("[U:1:11926250]"), Converter.to_steamID64("STEAM_0:0:5963125"))
+        self.assertEqual(Converter.to_steamID("76561197972191978"), "STEAM_0:0:5963125") # Test a known conversion
+        self.assertEqual(Converter.to_steamID("76561198054518843"), "STEAM_0:1:47126557")
+
+        self.assertEqual(Converter.to_steamID("76561197972191978"), Converter.to_steamID("[U:1:11926250]"))   # Corresponding steamID3 and SteamID
+        self.assertEqual(Converter.to_steamID("76561198054518843"), Converter.to_steamID("[U:1:94253115]")) 
+    
+
+    def test_steamID3(self): # Test conversion to steamID3
+
+        self.assertEqual(Converter.to_steamID3("76561197972191978"), "[U:1:11926250]") # Test a known conversion
+        self.assertEqual(Converter.to_steamID3("76561198054518843"), "[U:1:94253115]")
+
+        self.assertEqual(Converter.to_steamID3("76561197972191978"), Converter.to_steamID3("STEAM_0:0:5963125")) # Corresponding steamID3 and SteamID
+        self.assertEqual(Converter.to_steamID3("76561198054518843"), Converter.to_steamID3("STEAM_0:1:47126557")) 
+        
+
+    def test_steamID64(self): # Test conversion to steamID64
 
         self.assertEqual(Converter.to_steamID64("[U:1:94253115]"), "76561198054518843") # Test a known conversion
         self.assertEqual(Converter.to_steamID64("[U:1:11926250]"), "76561197972191978")
 
-    def test_steamID3(self): # Test conversion to steamID3
+        self.assertEqual(Converter.to_steamID64("[U:1:94253115]"), Converter.to_steamID64("STEAM_0:1:47126557")) # Corresponding steamID3 and SteamID
+        self.assertEqual(Converter.to_steamID64("[U:1:11926250]"), Converter.to_steamID64("STEAM_0:0:5963125"))   
 
-        self.assertEqual(Converter.to_steamID3("76561198054518843"), Converter.to_steamID3("STEAM_0:1:47126557")) # Corresponding steamID3 and SteamID
-        self.assertEqual(Converter.to_steamID3("76561197972191978"), Converter.to_steamID3("STEAM_0:0:5963125"))
-
-        self.assertEqual(Converter.to_steamID3("STEAM_0:0:5963125"), "[U:1:11926250]") # Test a known conversion
-        self.assertEqual(Converter.to_steamID3("STEAM_0:1:47126557"), "[U:1:94253115]")
-
+class TestHelpers(unittest.TestCase):
+    # Check other functions in Converter.py are working
+    
+    def test_steamID64_length(self):
+        # Raises error if steamID64 is not 17 digits, does nothing otherwise
+        self.assertRaises(ValueError, Converter.check_steamID64_length, "1"*15)
+        self.assertIsNone(Converter.check_steamID64_length("1"*17))
 
 if __name__ == '__main__':
     unittest.main()

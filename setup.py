300c5383d97a673128d7e1223e781ef934fb2fef
Alex Hodgson <56037599+AlexHodgson@users.noreply.github.com>
2022-11-03 21:04:19 +0100

    Version number update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="steamid-converter",
-    version="1.0.0",
+    version="1.0.1",
     author="Alex Hodgson",
     author_email="alex.hodgson@sci-fact.com",
     description="Package for easy conversion between steamID formats",
